e22396289eace2b6c8ff034fe661e467befa699b
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-04-10 19:42:56 +0900

    update devDependencies
    * introduce rollup-config-module to adapt to .mjs-based ecosystem
    * update ESLint from v3 to v4
    * introduce @shinnn/eslint-config and follow its coding rules, for exmaple tab indent
    * switch from istanbul to nyc

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
-/*!
- * array-includes-all | MIT (c) Shinnosuke Watanabe
- * https://github.com/shinnn/array-includes-all
-*/
 'use strict';
 
-module.exports = function arrayIncludesAll(arr, searchElms, fromIndex) {
-  if (!Array.isArray(arr)) {
-    throw new TypeError(
-      String(arr) +
-      ' is not an array.' +
-      ' The first argument of array-includes-all must be an array.'
-    );
-  }
+/*!
+* array-includes-all | MIT (c) Shinnosuke Watanabe
+* https://github.com/shinnn/array-includes-all
+*/
+function arrayIncludesAll(arr, searchElms, fromIndex) {
+	if (!Array.isArray(arr)) {
+		throw new TypeError(
+			String(arr) +
+			' is not an array.' +
+			' The first argument of array-includes-all must be an array.'
+		);
+	}
 
-  if (!Array.isArray(searchElms)) {
-    throw new TypeError(
-      String(searchElms) +
-      ' is not an array.' +
-      ' The second argument of array-includes-all must be an array.'
-    );
-  }
+	if (!Array.isArray(searchElms)) {
+		throw new TypeError(
+			String(searchElms) +
+			' is not an array.' +
+			' The second argument of array-includes-all must be an array.'
+		);
+	}
 
-  if (searchElms.length === 0) {
-    throw new RangeError(
-      'The second argument of array-includes-all must include at least one value, ' +
-      'but recieved an empty array.'
-    );
-  }
+	if (searchElms.length === 0) {
+		throw new RangeError(
+			'The second argument of array-includes-all must include at least one value, ' +
+			'but recieved an empty array.'
+		);
+	}
 
-  if (arr.length === 0) {
-    return false;
-  }
+	if (arr.length === 0) {
+		return false;
+	}
 
-  return searchElms.every(function checkIfArrIncludesElm(elm) {
-    return arr.includes(elm, fromIndex);
-  });
-};
+	return searchElms.every(function checkIfArrIncludesElm(elm) {
+		return arr.includes(elm, fromIndex);
+	});
+}
+
+module.exports = arrayIncludesAll;
--- test.js
+++ test.js
@@ -4,47 +4,47 @@ const arrayIncludesAll = require('.');
 const test = require('tape');
 
 test('arrayIncludesAll()', t => {
-  t.equal(
-    arrayIncludesAll([1, 2, 3], [1, 2]),
-    true,
-    'should return true if a given array includes all values.'
-  );
+	t.equal(
+		arrayIncludesAll([1, 2, 3], [1, 2]),
+		true,
+		'should return true if a given array includes all values.'
+	);
 
-  t.equal(
-    arrayIncludesAll([1, 2, 3], [1, 2, 4]),
-    false,
-    'should return true if a given array doesn\'t include all values.'
-  );
+	t.equal(
+		arrayIncludesAll([1, 2, 3], [1, 2, 4]),
+		false,
+		'should return true if a given array doesn\'t include all values.'
+	);
 
-  t.equal(
-    arrayIncludesAll([1, 2], [1, 2], 1),
-    false,
-    'should support the third `fromIndex` argument.'
-  );
+	t.equal(
+		arrayIncludesAll([1, 2], [1, 2], 1),
+		false,
+		'should support the third `fromIndex` argument.'
+	);
 
-  t.equal(
-    arrayIncludesAll([], [1]),
-    false,
-    'should return false if the first argument is an empty array.'
-  );
+	t.equal(
+		arrayIncludesAll([], [1]),
+		false,
+		'should return false if the first argument is an empty array.'
+	);
 
-  t.throws(
-    () => arrayIncludesAll(1, [1]),
-    /TypeError.*1 is not an array\. The first argument of array-includes-all must be an array\./,
-    'should throw a type error when the first argument is not an array.'
-  );
+	t.throws(
+		() => arrayIncludesAll(1, [1]),
+		/TypeError.*1 is not an array\. The first argument of array-includes-all must be an array\./,
+		'should throw a type error when the first argument is not an array.'
+	);
 
-  t.throws(
-    () => arrayIncludesAll([1], '1'),
-    /TypeError.*1 is not an array\. The second argument of array-includes-all must be an array\./,
-    'should throw a type error when the second argument is not an array.'
-  );
+	t.throws(
+		() => arrayIncludesAll([1], '1'),
+		/TypeError.*1 is not an array\. The second argument of array-includes-all must be an array\./,
+		'should throw a type error when the second argument is not an array.'
+	);
 
-  t.throws(
-    () => arrayIncludesAll([1], []),
-    /RangeError:.*must include at least one value, but recieved an empty array\./,
-    'should throw a range error when the second argument is an empty array.'
-  );
+	t.throws(
+		() => arrayIncludesAll([1], []),
+		/RangeError:.*must include at least one value, but recieved an empty array\./,
+		'should throw a range error when the second argument is an empty array.'
+	);
 
-  t.end();
+	t.end();
 });
